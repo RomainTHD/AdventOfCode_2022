ef8462ef16a3421e63608466633088f853b148c7
RomainTHD <romain-thd+github@protonmail.com>
2022-12-04 00:45:22 +0100

    feat: day 1

--- src/solutions/day1.ts
+++ src/solutions/day1.ts
@@ -1,9 +1,38 @@
-import { Observable } from "rxjs";
-import { Solution, Challenge, Input } from "./meta/challenge";
+import { Solution, Challenge, RawInput } from "./meta/challenge";
 
-export default class Day1 extends Challenge {
-	public override part1(input: Input): Solution | Observable<Solution> {
-		void input;
-		return 0;
+type Input = (number | null)[];
+
+export default class Day1 extends Challenge<Input> {
+	public override transform(rawInput: RawInput): Input {
+		return rawInput.map((line) => (line === "" ? null : parseInt(line, 10)));
+	}
+
+	public override part1(input: Input): Solution {
+		let acc = 0;
+		let max = 0;
+		input.forEach((elt) => {
+			if (elt === null) {
+				max = Math.max(max, acc);
+				acc = 0;
+			} else {
+				acc += elt;
+			}
+		});
+		return max;
+	}
+
+	public override part2(input: Input): Solution {
+		let acc = 0;
+		let maximas = [] as number[];
+		input.forEach((elt) => {
+			if (elt === null) {
+				maximas.push(Math.max(acc));
+				acc = 0;
+			} else {
+				acc += elt;
+			}
+		});
+		maximas = maximas.sort((a, b) => a - b);
+		return maximas[maximas.length - 1] + maximas[maximas.length - 2] + maximas[maximas.length - 3];
 	}
 }
